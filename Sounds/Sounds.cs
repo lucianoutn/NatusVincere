6ec8c5ef5042c475103a9899198423de108d77b9
martinvergara26 <martin.vergara@tictapps.com>
2016-06-06 23:58:49 -0300

    Sounds fixes

--- Sounds/Sounds.cs
+++ Sounds/Sounds.cs
@@ -9,41 +9,75 @@ namespace AlumnoEjemplos.NatusVincere
     class Sounds
     {
         MediaPlayer mediaPlayer = new MediaPlayer();
-        String musicPath = "AlumnoEjemplos\\NatusVincere\\music.mp3";
+        String happyMusicPath = "AlumnoEjemplos\\NatusVincere\\happy.mp3";
         String windPath = "AlumnoEjemplos\\NatusVincere\\wind.mp3";
-        String victoriaPath = "AlumnoEjemplos\\NatusVincere\\victoria.mp3";
+        String victoriaPath = "AlumnoEjemplos\\NatusVincere\\victory.mp3";
         String gameOverPath = "AlumnoEjemplos\\NatusVincere\\gameOver.mp3";
-        String talarArbolPath = "AlumnoEjemplos\\NatusVincere\\talarArbol.mp3";
+        String talarArbolPath = "AlumnoEjemplos\\NatusVincere\\talar.mp3";
+        String intenseMusicPath = "AlumnoEjemplos\\NatusVincere\\intense.mp3";
 
-        String musicAlias = "music";
+        String happyMusicAlias = "music";
         String windAlias = "alias";
+        String victoriaAlias = "victory";
+        String gameOverAlias = "gameOver";
+        String talarArbolAlias = "talar";
+        String intenseAlias = "intense";
+
+        Boolean isMainMusicOpened = false;
+        Boolean isWindPlaying = false;
 
         public void playMusic()
         {
-            mediaPlayer.FileName = musicPath;
-            mediaPlayer.play(true, musicPath, musicAlias);
+            if (isMainMusicOpened)
+            {
+                mediaPlayer.resume(happyMusicAlias);
+            }
+            
+            mediaPlayer.openAndPlay(true, happyMusicPath, happyMusicAlias);
+            isMainMusicOpened = true;
         }
-
+        
+        public void playIntense()
+        {
+            mediaPlayer.pause(happyMusicAlias);
+            mediaPlayer.openAndPlay(true, happyMusicPath, happyMusicAlias);
+        }
+        
         public void playViento()
         {
-            mediaPlayer.FileName = windPath;
-            mediaPlayer.play(true, windPath, windAlias);
-        
+            if (!isWindPlaying)
+            {
+                mediaPlayer.openAndPlay(true, windPath, windAlias);
+                isWindPlaying = true;
+            } 
+        }
+
+        public void stopViento()
+        {
+            if (isWindPlaying)
+            {
+                mediaPlayer.closeFile(windAlias);
+                isWindPlaying = false;
+            }
         }
 
         public void playVictoria()
         {
-            ;
+            mediaPlayer.closeFile("music");
+            isMainMusicOpened = false;
+            mediaPlayer.openAndPlay(true, victoriaPath, victoriaAlias);
         }
 
         public void playGameOver()
         {
-            ;
+            mediaPlayer.closeFile("music");
+            isMainMusicOpened = false;
+            mediaPlayer.openAndPlay(true, gameOverPath, gameOverAlias);
         }
 
         public void playTalarArbol()
         {
-            ;
+            mediaPlayer.openAndPlay(false, talarArbolPath, talarArbolAlias);
         }
     }
 }
